525abf5452a3b0eb5279cf5bce06bfe2684ef31e
Joe Monem <jmonem@icloud.com>
2023-10-06 14:31:40 +0300

    Initial Commit

---
+++ UrgeSuppressor/UrgeSuppressorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrgeSuppressorApp.swift
+//  UrgeSuppressor
+//
+//  Created by Joe Monem on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct UrgeSuppressorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
